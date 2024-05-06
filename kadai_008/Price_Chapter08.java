3626f3c2748c6071b724547814a11450b7c69030
toramike <160765408+toramike@users.noreply.github.com>
2024-05-06 14:44:49 +0900

    Add files via upload

---
+++ kadai_008/Price_Chapter08.java
@@ -0,0 +1,26 @@
+package kadai_008;
+
+public class Price_Chapter08 {
+
+	public static void main(String[] args) {
+		
+		int userAge = 30;
+		int serviceCost = 0;
+		
+		serviceCost = switch(userAge) {
+		case 10 -> 1000;
+		case 20 -> 2000;
+		case 30 -> 3000;
+		case 40 -> 3000;
+		case 50 -> 4000;
+		case 60 -> 4000;
+		case 70 -> 4000;
+		case 80 -> 5000;
+		default -> 500;
+		};
+		
+		System.out.println(userAge + "代の料金は" + serviceCost + "円");
+
+	}
+
+}
